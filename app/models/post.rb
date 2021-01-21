f35bfbef81c405ac11511f498a167e64a89e31fd
Ian Cossentino <icossentino@gmail.com>
2021-01-21 10:13:38 -0500

    commit tree 82d37bb0c184156cb9e95f18c826d07169894758

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ActiveRecord::Base
-  
+    has_many :post_tags
+    has_many :tags, through: :post_tags
+    belongs_to :user
+    validates_presence_of :name, :content
+
+
+    def tag_ids=(tag_ids_array)
+        tag_ids_array.each do |tag_id|
+            self.post_tags.build(tag_id: tag_id)
+        end
+    end
 end
